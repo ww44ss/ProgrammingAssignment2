8d221e7e47377613956292e16dde84e49116da72
Winston Saunders <winstonasaunders@gmail.com>
2014-05-21 22:50:08 -0700

    comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,20 +3,12 @@
 ##(there are also alternatives to matrix inversion that we will not discuss here). 
 ##Your assignment is to write a pair of functions that cache the inverse of a matrix.
 
-##Write the following functions:
-        
-###        makeCacheMatrix: This function creates a special "matrix" object 
-##         that can cache its inverse.
-###        cacheSolve: This function computes the inverse of the special "matrix" returned 
-##         by makeCacheMatrix above. If the inverse has already been calculated 
-##         (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
-##         Computing the inverse of a square matrix can be done with the solve function in R. 
-##         For example, if X is a square invertible matrix, then solve(X) returns its inverse.
-
-##         For this assignment, assume that the matrix supplied is always invertible.
+##         Assume that the matrix supplied is always invertible.
 
 
 makeCacheMatrix <- function(x = matrix()) {
+        ###        makeCacheMatrix: This function creates a special "matrix" object 
+        ##         that can cache its inverse.
                 m <- NULL
                 set <- function(y) {
                         x <<- y
@@ -31,6 +23,9 @@ makeCacheMatrix <- function(x = matrix()) {
         }
         
         cacheSolve <- function(x, ...) {
+                ###        cacheSolve: This function computes the inverse of the special "matrix" returned 
+                ##         by makeCacheMatrix above. If the inverse has already been calculated 
+                ##         (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
                 m <- x$getinverse()
                 if(!is.null(m)) {
                         message("getting cached inverse matrix")
@@ -44,6 +39,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ##      Here is an example usage
+##
 ##      > mm <- matrix(rnorm(16), nrow=4)
 ##      > yy<-makeCacheMatrix(mm)
 ##      > zz<-cacheSolve(yy)
